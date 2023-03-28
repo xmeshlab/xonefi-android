8ee0ab84aa7b42737643143e721c51b0a9751dad
Sungyu Kwon <kwonsun7@msu.edu>
2023-03-27 20:42:15 -0400

    Components for input bar and blue button

---
+++ navigation/Components/BigBlueButton.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View, Text} from 'react-native';
+import { PrimaryBtn } from '../../utils/components/PrimaryBtn';
+
+function BigBlueButton({text, onPressFunction}) {
+  return (
+    <View className="mx-5 flex flex-row justify-center my-2">
+        <PrimaryBtn className="flex-1" onPress={onPressFunction}>
+            <Text className="text-white text-sm m-1">{text}</Text>
+        </PrimaryBtn>
+    </View>
+  )
+}
+
+export default BigBlueButton
---
+++ navigation/Components/GreyTextInputBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useCallback, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+function GreyTextInputBar({placeholder_text, state_function}) {
+    const onTextInputChangeText = useCallback(text => {
+        state_function(text);
+    }, [])
+
+  return (
+    <View className="rounded-md border-slate-600 bg-slate-600 pl-3 pr-3 py-1 mt-2 mb-2 mx-6">
+        <TextInput selectionColor="#FFF"
+                onChangeText={onTextInputChangeText}
+                placeholderTextColor="#FFF" placeholder={placeholder_text}/>
+    </View>
+  )
+}
+
+export default GreyTextInputBar
