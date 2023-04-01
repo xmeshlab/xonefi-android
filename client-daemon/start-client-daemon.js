18292061645b878cae120e54ff1ea2715573c0a8
Nick Ivanov <sm230@pm.me>
2023-04-01 16:55:01 -0400

    update

--- client-daemon/start-client-daemon.js
+++ client-daemon/start-client-daemon.js
@@ -16,6 +16,10 @@ export async function startClientDaemon() {
     config_json.account_set = true;
     config_json.account.address = "0x0221B57Cc38C0360f1CAf638e1671243870C0424";
 
+    write_default_config(config_json, () => {
+        console.log("XLOG: Config is successfully initialized.")
+    });
+
     // read_default_config(() => {
     //     //console.log(`config_json: ${JSON.stringify(config_json)}`);
     //     config_json.version = "0.35";
@@ -35,28 +39,23 @@ export async function startClientDaemon() {
 
             read_default_config((config_json1) => {
                 //console.log(`config_json: ${JSON.stringify(config_json)}`);
-                console.log(`config_json: ${JSON.stringify(config_json1)}`);
+                console.log(`config_json1: ${JSON.stringify(config_json1)}`);
+
+                if(config_json1.client_on === true) {
+                    console.log("XLOG: config_json.client_session.status pre: " + config_json1.client_session.status);
+                    worker.client_worker(config_json1, user_password, decrypted_private_key, () => {
+                        console.log(`${global_counter}: Client is on`);
+                    });
+                    console.log("XLOG: config_json.client_session.status post: " + config_json.client_session.status);
+
+                } else {
+                    console.log(`${global_counter}: Client is off`);
+                }
+
+                global_counter++;
             });
-
-
-
-            if(config_json.client_on === true) {
-                console.log("XLOG: config_json.client_session.status pre: " + config_json.client_session.status);
-                worker.client_worker(config_json, user_password, decrypted_private_key, () => {
-                    console.log(`${global_counter}: Client is on`);
-                });
-                console.log("XLOG: config_json.client_session.status post: " + config_json.client_session.status);
-
-            } else {
-                console.log(`${global_counter}: Client is off`);
-            }
-
-            global_counter++;
         },
         5000);
-
-
-
 }
 
 module.exports = { startClientDaemon };
--- xonefi-api-client/ssid.js
+++ xonefi-api-client/ssid.js
@@ -18,128 +18,128 @@ along with OneFi Router.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 
-/**
- * Scan for Wi-Fi SSIDs
- * @param {string} iface - 802.11 interface
- * @param {function} callback Passes the object consisting of SSIDs and corresponding RSSI levels (ranging 0..255)
- */
-function ssid_scan(iface, callback) {
-    var wifi = require('node-wifi');
-
-    wifi.init({
-        iface: iface
-    });
-    
-    wifi.scan((error, networks) => {
-        if (error) {
-            console.log("Continue scanning...");
-        } else {
-            console.log(`Scan succeeded!`);
-            let ssids = [];
-            let rssis = [];
-
-            for(x of networks) {
-                //console.log(`[147aef5eaa]: x.ssid: '${x.ssid}'`);
-                if (x.ssid !== '') {
-                    ssids.push(x.ssid);
-                    //console.log(`[147aef5eaa]: x.signal_level: '${x.signal_level}'`);
-                    rssis.push(
-                        2.55 * 2 * (Number(x.signal_level) + 100)
-                    );
-                }
-            }
-
-            return callback({ssids, rssis});
-        }
-    });
-}
-
-
-/**
- * Experimental version of the scanner for Wi-Fi SSIDs
- * @param {string} iface - 802.11 interface
- * @param {function} callback Passes the object consisting of SSIDs and corresponding RSSI levels (ranging 0..255)
- */
-function ssid_scan_experimental(iface, callback) {
-    var iwlist = require('wireless-tools/iwlist');
-
-    iwlist.scan({ iface : iface, show_hidden : true }, function(err, networks) {
-        console.log(networks);
-        return callback(networks);
-    });
-}
-
-
-/**
- * Create a list of HTML select options consisting of the list of OneFi SSIDs available. This call must
- * be obsoleted in the future.
- * @param {array} networks - list of unfiltered networks returned by ssid_scan()
- * @returns {string} HTML code representing the list.
- */
-function prepare_ssid_html(networks) {
-    // TODO: remove this functionality from the API stack.
-    var options = "";
-
-    for (x of filter_onefi_ssids(networks)) {
-        options += `<option value="${x}">${x}</option>`;
-    }
-
-    return options;
-}
-
-
-/**
- * Store SSIDs in the state/configuration
- * @param {array} ssids - list of SSIDs
- * @returns {boolean} true: success; false: failure.
- */
-function save_ssids(ssids) {
-    const config = require("./config");
-    let config_json = config.read_default_config();
-    config_json.ssids = ssids;
-    config.write_default_config(config_json);
-    return true;
-}
-
-
-/**
- * Experimental version of the routine for storing SSIDs in the state/configuration
- * @param {array} ssids - list of networks as returned by ssid_scan()
- * @returns {boolean} true: success; false: failure.
- */
-function save_ssids_experimental(networks) {
-    const config = require("./config");
-    let config_json = config.read_default_config();
-    config_json.ssids = networks.map(x => x.ssid);
-    config.write_default_config(config_json);
-    return true;
-}
-
-
-/**
- * Serialize hotspot parameters into a Base64-encoded OneFi SSID format.
- * @param {Object} ssid_json - JSON object that consists of major parameters of OneFi provider (hotspot).
- * @returns {string} Base64-encoded OneFi SSID (see developer documentation for more information).
- */
-function serialize_ssid(ssid_json) {
-    var protocol_version_hex = ssid_json.protocol_version.toString(16).padStart(2, "0");
-    var hotspot_type_hex = ssid_json.hotspot_type.toString(16).padStart(2, "0");
-    var downlink_hex = ssid_json.downlink.toString(16).padStart(2, "0");
-    var uplink_hex = ssid_json.uplink.toString(16).padStart(2, "0");
-    var ip_hex = ip_quartet_to_number(ssid_json.ip).toString(16).padStart(8, "0");;
-    var port_hex = ssid_json.port.toString(16).padStart(4, "0");
-    var cost_hex = ssid_json.cost.toString(16).padStart(8, "0");
-    var pafren_hex = ssid_json.pafren.toString(16).padStart(4, "0");
-    var prefix_hex = ssid_json.prefix;
-
-    var ss = protocol_version_hex + hotspot_type_hex + downlink_hex
-        + uplink_hex + ip_hex + port_hex + cost_hex
-        + pafren_hex + prefix_hex;
-
-    var base64_part = Buffer.from(ss, 'hex').toString('base64');
-
-    return "OF" + base64_part;
-}
+// /**
+//  * Scan for Wi-Fi SSIDs
+//  * @param {string} iface - 802.11 interface
+//  * @param {function} callback Passes the object consisting of SSIDs and corresponding RSSI levels (ranging 0..255)
+//  */
+// function ssid_scan(iface, callback) {
+//     var wifi = require('node-wifi');
+//
+//     wifi.init({
+//         iface: iface
+//     });
+//
+//     wifi.scan((error, networks) => {
+//         if (error) {
+//             console.log("Continue scanning...");
+//         } else {
+//             console.log(`Scan succeeded!`);
+//             let ssids = [];
+//             let rssis = [];
+//
+//             for(x of networks) {
+//                 //console.log(`[147aef5eaa]: x.ssid: '${x.ssid}'`);
+//                 if (x.ssid !== '') {
+//                     ssids.push(x.ssid);
+//                     //console.log(`[147aef5eaa]: x.signal_level: '${x.signal_level}'`);
+//                     rssis.push(
+//                         2.55 * 2 * (Number(x.signal_level) + 100)
+//                     );
+//                 }
+//             }
+//
+//             return callback({ssids, rssis});
+//         }
+//     });
+// }
+//
+//
+// /**
+//  * Experimental version of the scanner for Wi-Fi SSIDs
+//  * @param {string} iface - 802.11 interface
+//  * @param {function} callback Passes the object consisting of SSIDs and corresponding RSSI levels (ranging 0..255)
+//  */
+// function ssid_scan_experimental(iface, callback) {
+//     var iwlist = require('wireless-tools/iwlist');
+//
+//     iwlist.scan({ iface : iface, show_hidden : true }, function(err, networks) {
+//         console.log(networks);
+//         return callback(networks);
+//     });
+// }
+//
+//
+// /**
+//  * Create a list of HTML select options consisting of the list of OneFi SSIDs available. This call must
+//  * be obsoleted in the future.
+//  * @param {array} networks - list of unfiltered networks returned by ssid_scan()
+//  * @returns {string} HTML code representing the list.
+//  */
+// function prepare_ssid_html(networks) {
+//     // TODO: remove this functionality from the API stack.
+//     var options = "";
+//
+//     for (x of filter_onefi_ssids(networks)) {
+//         options += `<option value="${x}">${x}</option>`;
+//     }
+//
+//     return options;
+// }
+//
+//
+// /**
+//  * Store SSIDs in the state/configuration
+//  * @param {array} ssids - list of SSIDs
+//  * @returns {boolean} true: success; false: failure.
+//  */
+// function save_ssids(ssids) {
+//     const config = require("./config");
+//     let config_json = config.read_default_config();
+//     config_json.ssids = ssids;
+//     config.write_default_config(config_json);
+//     return true;
+// }
+//
+//
+// /**
+//  * Experimental version of the routine for storing SSIDs in the state/configuration
+//  * @param {array} ssids - list of networks as returned by ssid_scan()
+//  * @returns {boolean} true: success; false: failure.
+//  */
+// function save_ssids_experimental(networks) {
+//     const config = require("./config");
+//     let config_json = config.read_default_config();
+//     config_json.ssids = networks.map(x => x.ssid);
+//     config.write_default_config(config_json);
+//     return true;
+// }
+//
+//
+// /**
+//  * Serialize hotspot parameters into a Base64-encoded OneFi SSID format.
+//  * @param {Object} ssid_json - JSON object that consists of major parameters of OneFi provider (hotspot).
+//  * @returns {string} Base64-encoded OneFi SSID (see developer documentation for more information).
+//  */
+// function serialize_ssid(ssid_json) {
+//     var protocol_version_hex = ssid_json.protocol_version.toString(16).padStart(2, "0");
+//     var hotspot_type_hex = ssid_json.hotspot_type.toString(16).padStart(2, "0");
+//     var downlink_hex = ssid_json.downlink.toString(16).padStart(2, "0");
+//     var uplink_hex = ssid_json.uplink.toString(16).padStart(2, "0");
+//     var ip_hex = ip_quartet_to_number(ssid_json.ip).toString(16).padStart(8, "0");;
+//     var port_hex = ssid_json.port.toString(16).padStart(4, "0");
+//     var cost_hex = ssid_json.cost.toString(16).padStart(8, "0");
+//     var pafren_hex = ssid_json.pafren.toString(16).padStart(4, "0");
+//     var prefix_hex = ssid_json.prefix;
+//
+//     var ss = protocol_version_hex + hotspot_type_hex + downlink_hex
+//         + uplink_hex + ip_hex + port_hex + cost_hex
+//         + pafren_hex + prefix_hex;
+//
+//     var base64_part = Buffer.from(ss, 'hex').toString('base64');
+//
+//     return "OF" + base64_part;
+// }
 
 
 /**
@@ -147,7 +147,7 @@ function serialize_ssid(ssid_json) {
  * @param {string} ssid - Base64-encoded OneFi SSID with serialized parameters (see dev. documentation for more information).
  * @returns {Object} JSON object with hostpot parameters.
  */
-function deserialize_ssid(ssid) {
+export function deserialize_ssid(ssid) {
     var base64_part = ssid.substring(2);
     var decoded_str = Buffer.from(base64_part, 'base64').toString('hex');
     var protocol_version_hex = decoded_str.substring(0,2);
@@ -213,150 +213,154 @@ function ip_number_to_quartet(ip_number) {
 }
 
 
-/**
- * Creates the hotspot parameters object that is used for serialization into the OneFi SSID.
- * @param {int} _protocol_version - integer version of the protocol
- * @param {int} _hotspot_type - encoded hotpost type (see developer documentation for more information)
- * @param {int} _downlink - downlink speed tier
- * @param {int} _uplink - uplink speed tier
- * @param {string} _ip - IPv4 address in the canonical format
- * @param {int} _port - UDP port
- * @param {string} _cost - cost per unit (either gigabyte or hour, depending on the mode)
- * @param {int} _pafren - PAFREN percentage
- * @param {string} _prefix - 10 lowercase leading symbols from the Ethereum address of the provider.
- * @returns {{protocol_version, cost, pafren, port, downlink, uplink, prefix, ip, hotspot_type}} JSON object suitable for
- * being used in serialization.
- */
-function compile_ssid_json(
-    _protocol_version,
-    _hotspot_type,
-    _downlink,
-    _uplink,
-    _ip,
-    _port,
-    _cost,
-    _pafren,
-    _prefix
-) {
-    let json = {
-        protocol_version: _protocol_version,
-        hotspot_type: _hotspot_type,
-        downlink: _downlink,
-        uplink: _uplink,
-        ip: _ip,
-        port: _port,
-        cost: _cost,
-        pafren: _pafren,
-        prefix: _prefix
-    };
+// /**
+//  * Creates the hotspot parameters object that is used for serialization into the OneFi SSID.
+//  * @param {int} _protocol_version - integer version of the protocol
+//  * @param {int} _hotspot_type - encoded hotpost type (see developer documentation for more information)
+//  * @param {int} _downlink - downlink speed tier
+//  * @param {int} _uplink - uplink speed tier
+//  * @param {string} _ip - IPv4 address in the canonical format
+//  * @param {int} _port - UDP port
+//  * @param {string} _cost - cost per unit (either gigabyte or hour, depending on the mode)
+//  * @param {int} _pafren - PAFREN percentage
+//  * @param {string} _prefix - 10 lowercase leading symbols from the Ethereum address of the provider.
+//  * @returns {{protocol_version, cost, pafren, port, downlink, uplink, prefix, ip, hotspot_type}} JSON object suitable for
+//  * being used in serialization.
+//  */
+// function compile_ssid_json(
+//     _protocol_version,
+//     _hotspot_type,
+//     _downlink,
+//     _uplink,
+//     _ip,
+//     _port,
+//     _cost,
+//     _pafren,
+//     _prefix
+// ) {
+//     let json = {
+//         protocol_version: _protocol_version,
+//         hotspot_type: _hotspot_type,
+//         downlink: _downlink,
+//         uplink: _uplink,
+//         ip: _ip,
+//         port: _port,
+//         cost: _cost,
+//         pafren: _pafren,
+//         prefix: _prefix
+//     };
+//
+//     return json;
+// }
+//
+// /**
+//  * Generate OneFi SSID using the data from the state/config of the hotspot.
+//  * @returns {string} Serialized Base64-encoded OneFi SSID string.
+//  */
+// function generate_ssid() {
+//     const config = require("./config");
+//     let config_json = config.read_default_config();
+//     const account = require("./account");
+//     const hotspot_type = require("./hotspot-type");
+//
+//     var cost = 0;
+//
+//     if(config_json.cft) {
+//         cost = config_json.price_ofi_hr;
+//     } else if(config_json.cfd) {
+//         cost = config_json.price_ofi_mb;
+//     } else {
+//         cost = 0;
+//     }
+//
+//     let ssid_json = compile_ssid_json(
+//         0,
+//         hotspot_type.calculate_hotspot_type(true),
+//         config_json.min_downlink_tier,
+//         config_json.min_uplink_tier,
+//         config_json.provider_ip,
+//         config_json.port,
+//         cost,
+//         config_json.pafren_percentage,
+//         account.get_account_raw_prefix()
+//     );
+//
+//     return serialize_ssid(ssid_json);
+// }
+//
+// /**
+//  * Check if the given SSID matches the OneFi format.
+//  * @param {string} ssid - SSID candidate
+//  * @returns {boolean} true: matches, false: doesn't match.
+//  */
+// function is_onefi_ssid(ssid) {
+//     if(ssid.length > 2) {
+//         if(ssid[0] === 'O' && ssid[1] === 'F') {
+//             let base64_part = ssid.substring(2);
+//             let decoded_str = Buffer.from(base64_part, 'base64').toString('hex');
+//             return decoded_str.length === 42;
+//         }
+//     }
+//
+//     return false;
+// }
+//
+// /**
+//  * Filter OneFi networks out all all scanned networks.
+//  * @param {Object} networks - JSON object consisting two arrays: ssids and rssis
+//  * @returns {Object} Filtered list of OneFi networks in the JSON object format consisting two
+//  * arrays: ssids and rssis.
+//  */
+// function filter_onefi_neworks(networks) {
+//     let res = {ssids: [], rssis: [] };
+//
+//     for(let i = 0; i < networks.ssids.length; i++) {
+//         let x = networks.ssids[i];
+//
+//         if(is_onefi_ssid(x)) {
+//             res.ssids.push(networks.ssids[i]);
+//             res.rssis.push(networks.rssis[i]);
+//         }
+//     }
+//
+//     return res;
+// }
+//
+//
+// /**
+//  * Filter OneFi SSIDs out of a given list of candidates.
+//  * @param {array} ssids - Array of SSIDs (candidates)
+//  * @returns {array} Filtered array of SSIDs matching the OneFi format.
+//  */
+// function filter_onefi_ssids(ssids) {
+//     let res = [];
+//
+//     for(let i = 0; i < ssids.length; i++) {
+//         let x = ssids[i];
+//         if(is_onefi_ssid(x)) {
+//             res.push(ssids[i]);
+//         }
+//     }
+//
+//     return res;
+// }
 
-    return json;
-}
 
-/**
- * Generate OneFi SSID using the data from the state/config of the hotspot.
- * @returns {string} Serialized Base64-encoded OneFi SSID string.
- */
-function generate_ssid() {
-    const config = require("./config");
-    let config_json = config.read_default_config();
-    const account = require("./account");
-    const hotspot_type = require("./hotspot-type");
-
-    var cost = 0;
-
-    if(config_json.cft) {
-        cost = config_json.price_ofi_hr;
-    } else if(config_json.cfd) {
-        cost = config_json.price_ofi_mb;
-    } else {
-        cost = 0;
-    }
-
-    let ssid_json = compile_ssid_json(
-        0,
-        hotspot_type.calculate_hotspot_type(true),
-        config_json.min_downlink_tier,
-        config_json.min_uplink_tier,
-        config_json.provider_ip,
-        config_json.port,
-        cost,
-        config_json.pafren_percentage,
-        account.get_account_raw_prefix()
-    );
-
-    return serialize_ssid(ssid_json);
-}
-
-/**
- * Check if the given SSID matches the OneFi format.
- * @param {string} ssid - SSID candidate
- * @returns {boolean} true: matches, false: doesn't match.
- */
-function is_onefi_ssid(ssid) {
-    if(ssid.length > 2) {
-        if(ssid[0] === 'O' && ssid[1] === 'F') {
-            let base64_part = ssid.substring(2);
-            let decoded_str = Buffer.from(base64_part, 'base64').toString('hex');
-            return decoded_str.length === 42;
-        }
-    }
-
-    return false;
-}
-
-/**
- * Filter OneFi networks out all all scanned networks.
- * @param {Object} networks - JSON object consisting two arrays: ssids and rssis
- * @returns {Object} Filtered list of OneFi networks in the JSON object format consisting two
- * arrays: ssids and rssis.
- */
-function filter_onefi_neworks(networks) {
-    let res = {ssids: [], rssis: [] };
-
-    for(let i = 0; i < networks.ssids.length; i++) {
-        let x = networks.ssids[i];
-
-        if(is_onefi_ssid(x)) {
-            res.ssids.push(networks.ssids[i]);
-            res.rssis.push(networks.rssis[i]);
-        }
-    }
-
-    return res;
-}
-
-
-/**
- * Filter OneFi SSIDs out of a given list of candidates.
- * @param {array} ssids - Array of SSIDs (candidates)
- * @returns {array} Filtered array of SSIDs matching the OneFi format.
- */
-function filter_onefi_ssids(ssids) {
-    let res = [];
-
-    for(let i = 0; i < ssids.length; i++) {
-        let x = ssids[i];
-        if(is_onefi_ssid(x)) {
-            res.push(ssids[i]);
-        }
-    }
-
-    return res;
-}
-
+// module.exports = {
+//     ssid_scan,
+//     ssid_scan_experimental,
+//     prepare_ssid_html,
+//     save_ssids,
+//     save_ssids_experimental,
+//     serialize_ssid,
+//     deserialize_ssid,
+//     filter_onefi_ssids,
+//     compile_ssid_json,
+//     generate_ssid,
+//     is_onefi_ssid,
+//     filter_onefi_neworks
+// };
 
 module.exports = {
-    ssid_scan,
-    ssid_scan_experimental,
-    prepare_ssid_html,
-    save_ssids,
-    save_ssids_experimental,
-    serialize_ssid,
-    deserialize_ssid,
-    filter_onefi_ssids,
-    compile_ssid_json,
-    generate_ssid,
-    is_onefi_ssid,
-    filter_onefi_neworks
+    deserialize_ssid
 };
