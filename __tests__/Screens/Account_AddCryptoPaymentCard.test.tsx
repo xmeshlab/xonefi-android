ab3dd85cd267b3be7d9f295d8a675f3d2d03b045
Sungyuk1 <kwonsun7@msu.edu>
2023-05-26 11:16:48 -0400

    More Test Frameworks

---
+++ __tests__/Screens/Account_AddCryptoPaymentCard.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { describe, expect, test } from "@jest/globals";
+import { render, fireEvent } from "@testing-library/react-native";
+
+import AccountAddCrptoPaymentCard from "../../navigation/screens/Account_AddCryptoPaymentCard";
+
+it("renders", ()=>{
+    render(<AccountAddCrptoPaymentCard key={"Test"} name={"Test"}/>)
+})
---
+++ __tests__/Screens/ConnectScreen.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { describe, expect, test } from "@jest/globals";
+import { render, fireEvent } from "@testing-library/react-native";
+
+import AccountBankDetails from "../../navigation/screens/Account_BankDetails";
+import ConnectScreen from "../../navigation/screens/ConnectScreen";
+
+/*it("renders", ()=>{
+    render(<ConnectScreen/>)
+})*/
