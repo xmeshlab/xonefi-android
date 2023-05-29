c64ed35f49d4016bc723c1382846c87258919cf1
Sungyuk1 <kwonsun7@msu.edu>
2023-05-29 17:42:10 -0400

    Logic for displaying a different view if not logged in

--- App.tsx
+++ App.tsx
@@ -3,7 +3,11 @@ import { RootSiblingParent } from "react-native-root-siblings";
 import MainContainer from "./navigation/MainContainer";
 import BackgroundTimer from "react-native-background-timer";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import { useState } from "react";
+import {
+  View,
+  Text,
+} from "react-native";
 import {
   read_default_config,
   write_default_config,
@@ -12,18 +16,57 @@ import {
 } from "./xonefi-api-client/config";
 import { startClientDaemon } from "./client-daemon/start-client-daemon";
 
+import * as WebBrowser from "@toruslabs/react-native-web-browser";
+import Web3Auth, {
+  LOGIN_PROVIDER,
+  OPENLOGIN_NETWORK,
+} from "@web3auth/react-native-sdk";
+
+//web3Auth Code
+const scheme = "web3authrnexample"; // Or your desired app redirection scheme
+const resolvedRedirectUrl = `${scheme}://openlogin`;
+
+const clientId =
+  "BHU5wO49Ul-c13pLy6HT84KINj4fcQ20W_3H7dZWj5AP3LRWIE69ZjVVWZ3B0u_TkJx8TbPK6iFeK0gzf5is5Oo";
+
+const web3auth = new Web3Auth(WebBrowser, {
+  clientId,
+  network: OPENLOGIN_NETWORK.TESTNET, // or other networks
+});
+
 
 function App() {
   console.log("XLOG: App.tsx");
 
-  // var SQLite = require('react-native-sqlite-storage');
-  //
-  //
-  // var db = SQLite.openDatabase("test.db", "1.0", "Test Database", 200000, () => {
-  //     console.log("XLOG: Database opened");
-  // }, (err) => {
-  //     console.log("XLOG: Error opening database");
-  // });
+  const [key, setKey] = useState("");
+  const [userInfo, setUserInfo] = useState("");
+  //const [console, setConsole] = useState("");
+
+  const loginWithWeb3Auth = async () => {
+    console.log("Loggin in with Web3Auth");
+    try {
+      console.log("Loggin in with Web3Auth");
+      //setConsole("Logging in");
+      const web3auth = new Web3Auth(WebBrowser, {
+        clientId,
+        network: OPENLOGIN_NETWORK.TESTNET, // or other networks
+      });
+      console.log("web3auth object");
+      console.log(web3auth);
+      const info = await web3auth.login({
+        loginProvider: LOGIN_PROVIDER.GOOGLE,
+        redirectUrl: resolvedRedirectUrl,
+      });
+      console.log("info returned from web3 Auth");
+      console.log(info);
+
+      setUserInfo(info);
+      setKey(info.privKey);
+      //uiConsole("Logged In");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   config_init_if_absent((ret) => {
     if (ret === true) {
@@ -37,12 +80,24 @@ function App() {
     }
   });
 
-  return (
+  const loggedInView = (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <RootSiblingParent>
         <MainContainer />
       </RootSiblingParent>
     </GestureHandlerRootView>
+  )
+
+  const unloggedInView = (
+    <View>
+      <Text>Not Logged In</Text>
+    </View>
+  )
+
+  return (
+    <View>
+    {key ? loggedInView : unloggedInView}
+    </View>
   );
 }
 
