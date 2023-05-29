7e417d3a15b10b537e2fd6bacdb5a629fd9ece15
Sungyuk1 <kwonsun7@msu.edu>
2023-05-29 18:44:01 -0400

    Removing Code that is no longer needed. Web3 Auth code is now in main container

--- App.tsx
+++ App.tsx
@@ -3,11 +3,6 @@ import { RootSiblingParent } from "react-native-root-siblings";
 import MainContainer from "./navigation/MainContainer";
 import BackgroundTimer from "react-native-background-timer";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useState } from "react";
-import {
-  View,
-  Text,
-} from "react-native";
 import {
   read_default_config,
   write_default_config,
@@ -16,58 +11,8 @@ import {
 } from "./xonefi-api-client/config";
 import { startClientDaemon } from "./client-daemon/start-client-daemon";
 
-import * as WebBrowser from "@toruslabs/react-native-web-browser";
-import Web3Auth, {
-  LOGIN_PROVIDER,
-  OPENLOGIN_NETWORK,
-} from "@web3auth/react-native-sdk";
-import BigBlueButton from "./navigation/Components/BigBlueButton";
-
-//web3Auth Code
-const scheme = "web3authrnexample"; // Or your desired app redirection scheme
-const resolvedRedirectUrl = `${scheme}://openlogin`;
-
-const clientId =
-  "BHU5wO49Ul-c13pLy6HT84KINj4fcQ20W_3H7dZWj5AP3LRWIE69ZjVVWZ3B0u_TkJx8TbPK6iFeK0gzf5is5Oo";
-
-const web3auth = new Web3Auth(WebBrowser, {
-  clientId,
-  network: OPENLOGIN_NETWORK.TESTNET, // or other networks
-});
-
-
 function App() {
   console.log("XLOG: App.tsx");
-
-  const [key, setKey] = useState("");
-  const [userInfo, setUserInfo] = useState("");
-  //const [console, setConsole] = useState("");
-
-  const loginWithWeb3Auth = async () => {
-    console.log("Loggin in with Web3Auth");
-    try {
-      console.log("Loggin in with Web3Auth");
-      //setConsole("Logging in");
-      const web3auth = new Web3Auth(WebBrowser, {
-        clientId,
-        network: OPENLOGIN_NETWORK.TESTNET, // or other networks
-      });
-      console.log("web3auth object");
-      console.log(web3auth);
-      const info = await web3auth.login({
-        loginProvider: LOGIN_PROVIDER.GOOGLE,
-        redirectUrl: resolvedRedirectUrl,
-      });
-      console.log("info returned from web3 Auth");
-      console.log(info);
-
-      setUserInfo(info);
-      setKey(info.privKey);
-      //uiConsole("Logged In");
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
   config_init_if_absent((ret) => {
     if (ret === true) {
@@ -80,21 +25,6 @@ function App() {
       console.log("XLOG: Database initialization error.");
     }
   });
-
-  const loggedInView = (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <RootSiblingParent>
-        <MainContainer />
-      </RootSiblingParent>
-    </GestureHandlerRootView>
-  )
-
-  const unloggedInView = (
-    <View>
-      <Text>Not Logged In</Text>
-      <BigBlueButton text={"Log In"} onPressFunction={loginWithWeb3Auth}/>
-    </View>
-  )
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
--- navigation/screens/LinkedAccountsScreen.js
+++ navigation/screens/LinkedAccountsScreen.js
@@ -12,59 +12,11 @@ import GreyButton from "../Components/GreyButton";
 
 import { useCallback } from "react";
 
-import * as WebBrowser from "@toruslabs/react-native-web-browser";
-import Web3Auth, {
-  LOGIN_PROVIDER,
-  OPENLOGIN_NETWORK,
-} from "@web3auth/react-native-sdk";
-
-//web3Auth Code
-const scheme = "web3authrnexample"; // Or your desired app redirection scheme
-const resolvedRedirectUrl = `${scheme}://openlogin`;
-
-const clientId =
-  "BHU5wO49Ul-c13pLy6HT84KINj4fcQ20W_3H7dZWj5AP3LRWIE69ZjVVWZ3B0u_TkJx8TbPK6iFeK0gzf5is5Oo";
-
-const web3auth = new Web3Auth(WebBrowser, {
-  clientId,
-  network: OPENLOGIN_NETWORK.TESTNET, // or other networks
-});
-
 export default function LinkedAccountScreen({ navigation }) {
-  const [key, setKey] = useState("");
-  const [userInfo, setUserInfo] = useState("");
-  //const [console, setConsole] = useState("");
-
   const onTouchableOpacityPress = useCallback(
     () => navigation.navigate("Account Information"),
     []
   );
-
-  const loginWithWeb3Auth = async () => {
-    console.log("Loggin in with Web3Auth");
-    try {
-      console.log("Loggin in with Web3Auth");
-      //setConsole("Logging in");
-      const web3auth = new Web3Auth(WebBrowser, {
-        clientId,
-        network: OPENLOGIN_NETWORK.TESTNET, // or other networks
-      });
-      console.log("web3auth object");
-      console.log(web3auth);
-      const info = await web3auth.login({
-        loginProvider: LOGIN_PROVIDER.GOOGLE,
-        redirectUrl: resolvedRedirectUrl,
-      });
-      console.log("info returned from web3 Auth");
-      console.log(info);
-
-      setUserInfo(info);
-      setKey(info.privKey);
-      //uiConsole("Logged In");
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
   return (
     //<View className="flex-1 flex-col bg-black pt-5">
@@ -91,18 +43,6 @@ export default function LinkedAccountScreen({ navigation }) {
         imageSource={LogoutIcon}
         textInput={"Logout"}
         onPressFunction={() => navigation.navigate("Logout")}
-      />
-      <GreyButton
-        imageSource={LogoutIcon}
-        textInput={"Login Using Web3Auth"}
-        onPressFunction={loginWithWeb3Auth}
-      />
-      <GreyButton
-        imageSource={LogoutIcon}
-        textInput={"Web3Auth - Show Private Key"}
-        onPressFunction={() => {
-          alert(key);
-        }}
       />
     </View>
   );
