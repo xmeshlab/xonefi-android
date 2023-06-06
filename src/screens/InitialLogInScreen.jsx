be3f0407608b5c4208c70eea8b8e9cfec51c9e80
Sungyuk1 <kwonsun7@msu.edu>
2023-06-06 15:50:18 -0400

    Google Icon

--- src/screens/InitialLogInScreen.jsx
+++ src/screens/InitialLogInScreen.jsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import BigBlueButton from '../Components/BigBlueButton'
 import XOneFi_Logo_Big from '../icons/XOneFi'
+import { GoogleIcon } from '../icons/sign_in_icon'
 
 const InitialLogInScreen = ({logInFunction}) => {
   return (
@@ -11,6 +12,7 @@ const InitialLogInScreen = ({logInFunction}) => {
         </View>
 
         <View className="absolute top-1/2 w-screen">
+                  <GoogleIcon/>
             <BigBlueButton text={"Log In"} onPressFunction={logInFunction}/>
             <BigBlueButton text={"Sign Up"} onPressFunction={()=>{alert("Sign Up Button Clicked")}}/>
         </View>
