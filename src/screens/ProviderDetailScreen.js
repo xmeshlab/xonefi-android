d7c72b32d15c3bd490b76e2a53c3f60a727b804a
Sungyuk1 <kwonsun7@msu.edu>
2023-06-12 00:40:15 -0400

    Provider Screen now uses GreyBackgroundBar Component

--- src/screens/ProviderDetailScreen.js
+++ src/screens/ProviderDetailScreen.js
@@ -6,41 +6,32 @@ import GreyBackgroundBar from "../Components/GreyBackgroundBar";
 
 //import providor component
 import Provider from "../Components/Provider";
-
-import backgroundImage from "../../assets/background.png";
+import GreyBackgroundBox from "../Components/GreyBackgroundBox";
 
 export default function ProviderDetailScreen({ navigation }) {
   return (
     <ScrollView>
-        <Text className="text-white text-xl mt-6 mb-1 mx-6">
-          Router Information
-        </Text>
-        <View className="flex flex-col ml-5 mr-5 bg-slate-800 bg-rounded p-5 rounded-2xl justify-around">
+        <GreyBackgroundBox titleText={"Router Information"} children={<>
           
           <GreyBackgroundBar LeftText={"Router Name"} RightSideComponent={<Text>Insert</Text>}/>
           <GreyBackgroundBar LeftText={"IP Address"} RightSideComponent={<Text>Insert</Text>}/>
           <GreyBackgroundBar LeftText={"Wifi-Speed"} RightSideComponent={<Text>Insert</Text>}/>
-        </View>
+        </>}/>
 
-        <Text className="text-white text-xl mt-3 mb-1 mx-6">Sharing Setting</Text>
-        <View className="flex flex-col ml-5 mr-5 bg-slate-800 bg-rounded p-5 rounded-2xl justify-around">
-        
+        <GreyBackgroundBox titleText={"Sharing  Setting"} children={<>
           <GreyBackgroundBar LeftText={"OFI/GB"}RightSideComponent={<ViewButton OnPressFunction={()=>{alert("Button Pressed")}}/>}/>
           <GreyBackgroundBar LeftText={'Private Connection'} RightSideComponent={<ViewButton OnPressFunction={()=>{alert("Button Pressed")}}/>}/>
           <GreyBackgroundBar LeftText={'Share Time/Daily'} RightSideComponent={<ViewButton OnPressFunction={()=>{alert("Share Times/Daily")}}/>}/>
-        </View>
+        </>}/>
 
-        <Text className="text-white text-xl mt-3 mb-1 mx-6">
-          Connected Clients
-        </Text>
-        <View className="flex flex-col ml-5 mr-5 bg-slate-800 bg-rounded p-5 rounded-2xl justify-around">
+        <GreyBackgroundBox titleText={"Connected Clients"} children={<>
           <View className="flex flex-row mb-2 justify-between">
             <Text className="text-white text-base">OFI/GB</Text>
             <TouchableOpacity className="rounded-md border-slate-600 bg-slate-600 pl-3 pr-3 py-1">
               <Text className="text-white">View</Text>
             </TouchableOpacity>
           </View>
-        </View>
+        </>}/>
       </ScrollView>
   );
 }
