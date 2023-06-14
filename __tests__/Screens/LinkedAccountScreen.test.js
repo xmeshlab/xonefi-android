79cd29975171f0d2b94d4884d5859ba7107dca05
Sungyuk1 <kwonsun7@msu.edu>
2023-06-14 15:31:44 -0400

    Commented out the areas that the previous developer was using ReactNativeAsyncStorage.

--- __tests__/Screens/LinkedAccountScreen.test.js
+++ __tests__/Screens/LinkedAccountScreen.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { describe, expect, test } from "@jest/globals";
 import { render, fireEvent } from "@testing-library/react-native";
 
 import LinkedAccountScreen from "../../src/screens/LinkedAccountsScreen";
--- src/screens/ConnectScreen.tsx
+++ src/screens/ConnectScreen.tsx
@@ -27,7 +27,24 @@ import { PrimaryBtn } from "../../utils/components/PrimaryBtn";
 import WifiLevelIcon from "../icons/WifiLevelIcon";
 import LockICon from "../icons/LockIcon";
 import { globalStyle } from "../../constants/globalStyle";
-import { is_onefi_ssid } from "../../api/client";
+
+//import { is_onefi_ssid } from "../../api/client";
+/**
+ * Check if the given SSID matches the OneFi format.
+ * @param {string} ssid - SSID candidate
+ * @returns {boolean} true: matches, false: doesn't match.
+ */
+function is_onefi_ssid(ssid: string) {
+  if (ssid.length > 2) {
+    if (ssid[0] === "O" && ssid[1] === "F") {
+      let base64_part = ssid.substring(2);
+      let decoded_str = Buffer.from(base64_part, "base64").toString("hex");
+      return decoded_str.length === 42;
+    }
+  }
+
+  return false;
+}
 
 import {
   read_default_config,
