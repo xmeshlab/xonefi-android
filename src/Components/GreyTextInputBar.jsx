a05cb9e9e7a3a8bf39373f694516346457b7e442
Sungyuk1 <kwonsun7@msu.edu>
2023-07-18 12:55:15 -0400

    Edited GreyTextInputBar and changed LinkedPaymentScreen UI as requested

--- src/Components/GreyTextInputBar.jsx
+++ src/Components/GreyTextInputBar.jsx
@@ -2,12 +2,48 @@ import React from "react";
 import { useCallback, useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
+/**
+ * A component for text input bar styled the way we want
+ * 
+ * @TODO maybe use onEndEditing instead of onChangeText to reduce refreshes
+ */
 function GreyTextInputBar({ placeholder_text, state_function }) {
   const onTextInputChangeText = useCallback((text) => {
     state_function(text);
   }, []);
 
   return (
+    <View className="flex flex-col rounded-2xl border-slate-600 bg-slate-800 bg-rounded pl-3 pr-3 p-2 ml-5 mr-5" testID="Grey Text Input Bar">
+      <View className="pl-3 pr-3">
+        <TextInput 
+          style={{color: 'white'}}
+          selectionColor="#FFF"
+          onChangeText={onTextInputChangeText}
+          placeholderTextColor="#808080"
+          placeholder={placeholder_text}
+          testID="Text Input"
+        />
+      </View>
+    </View>
+  );
+}
+
+export default GreyTextInputBar;
+
+{/*<View className="flex flex-col ml-5 mr-5 bg-slate-800 bg-rounded p-2 rounded-2xl justify-around">
+    <View className="flex flex-row  justify-between">
+      <View className="pl-3 pr-3">
+        <TextInput
+          selectionColor="#FFF"
+          placeholderTextColor="#FFF"
+          placeholder="John Doe"
+        />
+      </View>
+    </View>
+  </View>*/}
+
+/**
+ * return (
     <View className="rounded-md border-slate-600 bg-slate-600 pl-3 pr-3 py-1 mt-2 mb-2 mx-6" testID="Grey Text Input Bar">
       <TextInput
         selectionColor="#FFF"
@@ -18,6 +54,4 @@ function GreyTextInputBar({ placeholder_text, state_function }) {
       />
     </View>
   );
-}
-
-export default GreyTextInputBar;
+ */
---
+++ src/screens/LinkedPaymentCardScreen.jsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+import backgroundImage from "../../assets/background.png";
+import BigBlueButton from "../Components/BigBlueButton";
+import GreyTextInputBar from "../Components/GreyTextInputBar";
+
+export default function LinkedPaymentCardScreen({ navigation }) {
+  const [cardHolder, setCardHolder] = useState();
+  const [cardNumber, setCardNumber] = useState();
+  const [cardCSV, setCardCSV] = useState();
+  const [cardEXP, setCardEXP] = useState();
+
+  return (
+<View>
+      <Text className="text-white text-xl mt-6 mb-1 ml-7">
+        Add New Payment Card
+      </Text>
+      <Text className="pl-8 text-slate-400 text-base">Card Holder</Text>
+      <GreyTextInputBar placeholder_text={"Enter Full Name"} state_function={setCardHolder}/>
+
+      <Text className="pl-8 text-slate-400 text-base mt-2">Card Number</Text>
+      <GreyTextInputBar placeholder_text={"Enter Card Number"} state_function={setCardNumber}/>
+      
+
+      <View className="flex flex-row">
+        <View className="flex flex-col w-6/12">
+          <Text className="pl-8 text-slate-400 text-base mt-2">CSV</Text>
+          <GreyTextInputBar placeholder_text={"Enter CSV"} state_function={setCardCSV}/>
+        </View>
+
+        <View className="flex flex-col w-6/12">
+          <Text className="pl-8 text-slate-400 text-base mt-2">EXP</Text>
+          <GreyTextInputBar placeholder_text={"Enter EXP"} state_function={setCardEXP}/>
+      </View>
+      </View>
+      <View className="mt-3">
+        <BigBlueButton
+          text={"Add Payment Card"}
+          onPressFunction={() => {
+            alert("Button Pressed");
+          }}
+        />
+      </View>
+      </View>
+  );
+}
