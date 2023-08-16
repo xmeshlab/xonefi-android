77a4e8bc067aaeda9aabb2b925176b6052348124
Sungyuk1 <kwonsun7@msu.edu>
2023-08-16 02:21:09 -0400

    Edited text sizes and gaps in Provider Details Screen as requested

--- src/Components/GreyBackgroundBox.jsx
+++ src/Components/GreyBackgroundBox.jsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function GreyBackgroundBox({ titleText, children }) {
   return (
     <>
-      <Text className="text-white text-3xl mt-6 mb-8 mx-6">{titleText}</Text>
+      <Text className="text-white text-2xl mt-6 mb-3 mx-6">{titleText}</Text>
       <View className="flex flex-col ml-5 mr-5 bg-slate-800 bg-rounded p-5 rounded-2xl justify-around">
         {children}
       </View>
