99bf0504ad3f1806388baa7709d7aa1589ed2a21
Sungyuk1 <kwonsun7@msu.edu>
2023-11-30 15:16:16 -0500

    Adding Test for Button Press

--- __tests__/Components/Provider.test.jsx
+++ __tests__/Components/Provider.test.jsx
@@ -3,10 +3,9 @@ import { describe, expect, test } from "@jest/globals";
 import { render, fireEvent } from "@testing-library/react-native";
 
 import Provider from "../../src/Components/Provider.jsx";
-const navigationFunctionMock = jest.fn();
+const navigationFunctionMock = jest.fn().mockReturnValueOnce("Provider Pressed");
 
-//function Provider({ProviderName,UsersConnectedNumber,TokenGenNumber,Status,})
-test("Rendering the Provider Component", () => {
+it("Rendering the Provider Component", () => {
   const { getByText } = render(
     <Provider
       ProviderName={"Test Name"}
@@ -17,4 +16,20 @@ test("Rendering the Provider Component", () => {
 
   ProviderComponent = getByText("Test Name");
   expect(ProviderComponent).not.toBeNull();
+});
+
+it("Checking Button Press", () => {
+  const { getByText, getByTestId } = render(
+    <Provider
+      ProviderName={"Test Name"}
+      Status={true}
+      navigationFunction={navigationFunctionMock}
+    />
+  );
+
+  ProviderComponent = getByText("Test Name");
+  expect(ProviderComponent).not.toBeNull();
+
+  //Press all the buttons
+  expect(fireEvent.press(getByTestId("Provider Button"))).toBe("Provider Pressed");
 });
