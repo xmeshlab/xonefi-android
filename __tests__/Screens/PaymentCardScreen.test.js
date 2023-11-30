e22c72f6c4b5039edb8daf07ab5e8bb81494a8be
Sungyuk1 <kwonsun7@msu.edu>
2023-11-30 16:55:17 -0500

    Mocking react native clipboard and also deleting tests for unused files

--- __tests__/Screens/PaymentCardScreen.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { describe, expect, test } from "@jest/globals";
-import { render, fireEvent } from "@testing-library/react-native";
-
-import PaymentCardScreen from "../../src/screens/Currently Not in User Flow/PaymentCardScreen";
-
-it("renders", () => {
-  render(<PaymentCardScreen />);
-});
--- setupJest.js
+++ setupJest.js
@@ -1,3 +1,5 @@
+//import Web3 from "web3";
+
 //we just directly override the global.fetch function, which is what our app leverages to make remote requests.
 global.fetch = require("jest-fetch-mock");
 
@@ -8,5 +10,20 @@ jest.mock('@react-native-async-storage/async-storage', () =>
 
 jest.mock("react-native-device-info", () =>({
     getDeviceId: jest.fn().mockReturnValueOnce("1"),
-})
-);
+}))
+
+//mock the clipboard module
+jest.mock('@react-native-clipboard/clipboard', () =>({
+    setString: jest.fn(),
+}));
+
+//mock web3
+/*jest.mock("web3");
+Web3.mockImplementation((input)=>{
+    return{
+
+    }
+})*/
+/*jest.mock("web3", () =>({
+    Web3: jest.fn().mockImplementation((input)=>{})
+}));*/
