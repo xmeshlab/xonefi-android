f16bd0b0b6f6305fd3c5628eb6780888b6ff4d21
Sungyuk1 <kwonsun7@msu.edu>
2023-11-30 16:36:39 -0500

    Properly mocking react-native-async-storage and also mocking the module react-native-device-info since it uses native code. Fixes LinkedAccountScreen test

--- setupJest.js
+++ setupJest.js
@@ -1,2 +1,12 @@
 //we just directly override the global.fetch function, which is what our app leverages to make remote requests.
 global.fetch = require("jest-fetch-mock");
+
+//Mocking AsyncStorge
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+jest.mock("react-native-device-info", () =>({
+    getDeviceId: jest.fn().mockReturnValueOnce("1"),
+})
+);
