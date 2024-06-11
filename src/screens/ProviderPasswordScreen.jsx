e9faa8503468824a0d973deaad04dc35bc7785ac
Sungyuk1 <kwonsun7@msu.edu>
2024-06-11 16:09:41 -0400

    If statement for logo display depending on background color

--- src/screens/ProviderPasswordScreen.jsx
+++ src/screens/ProviderPasswordScreen.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react';
 import BigBlueButton from "../Components/BigBlueButton/BigBlueButton";
 import GreyTextInputBar from "../Components/GreyTextInputBar";
 import XOneFi_Logo_Big from "../icons/XOneFi";
+import { useUserContext } from "../context/UserContext";
 
 const ProviderPasswordScreen = ({setValidPasswordProvided}) => {
     const [currentPassword, setCurrentPassword] = useState("")
+    const userContext_array = useUserContext();
 
   return (
     <View className='flex flex-col justify-center flex-grow pb-10'>
         <View className='flex flex-row justify-center m-5'>
-            <XOneFi_Logo_Big />
+        {userContext_array[4] == "black" ? <XOneFi_Logo_Big /> : <View className='bg-black'><XOneFi_Logo_Big /></View>}
         </View>
       <GreyTextInputBar placeholder_text={" Enter Password"}
             state_function={setCurrentPassword}/>
