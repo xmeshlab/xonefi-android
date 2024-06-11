b3fd0d894c3c778511b486a70aaf39028c039fc5
Sungyuk1 <kwonsun7@msu.edu>
2024-06-11 16:45:38 -0400

    removing unused imports

--- src/Components/WifiItem/WifiItem.jsx
+++ src/Components/WifiItem/WifiItem.jsx
@@ -1,9 +1,4 @@
 import * as React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-} from "react-native";
 
 import WifiItemWhite from "./WifiItemWhite";
 import WifiItemBlack from "./WifiItemBlack";
